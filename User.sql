6dc2e965d333fb474947a0d6611d3a2e714d37c9
Jackson Bullard <jsbullar@uark.edu>
2021-04-16 20:38:32 -0500

    Initial commit. User database definitions.

---
+++ User.sql
@@ -0,0 +1,40 @@
+CREATE TABLE user
+(
+	id uuid NOT NULL DEFAULT gen_random_uuid(),
+	username character varying(32) NOT NULL DEFAULT(''),
+	password bytea NOT NULL DEFAULT(''),
+	active boolean NOT NULL DEFAULT(FALSE),
+	createdon timestamp without time zone NOT NULL DEFAULT now(),
+	CONSTRAINT user_pkey PRIMARY KEY (id)
+)
+WITH
+(
+	OIDS=FALSE
+);
+
+CREATE INDEX ix_user_username
+	ON user
+	USING btree(username);
+
+-----
+CREATE TABLE activeuser
+(
+	id uuid NOT NULL DEFAULT gen_random_uuid(),
+	userid uuid NOT NULL,
+	username character varying(32) NOT NULL DEFAULT(''),
+	sessionkey character varying(128) NOT NULL DEFAULT(''),
+	createdon timestamp without time zone NOT NULL DEFAULT now(),
+	CONSTRAINT activeuser_pkey PRIMARY KEY (id)
+)
+WITH
+(
+	OIDS=FALSE
+);
+
+CREATE INDEX ix_activeuser_userid
+	ON activeuser
+	USING btree(userid);
+
+CREATE INDEX ix_activeuser_sessionkey
+	ON activeuser
+	USING btree(sessionkey);
