e4e63085e081071d3719e292aa6e99ca0f0efb71
Arenile <55638040+Arenile@users.noreply.github.com>
2021-04-17 01:06:08 -0500

    Update ToDoList.sql

--- ToDoList.sql
+++ ToDoList.sql
@@ -1,12 +1,12 @@
 CREATE TABLE todolist
 (
-	id uuid NOT NULL,
-	entryid NOT NULL gen_random_uuid(),
+    entryid uuid NOT NULL DEFAULT gen_random_uuid(),
+	userid uuid NOT NULL,
 	createdon timestamp without time zone NOT NULL DEFAULT now(),
 	suggester character varying(32) NOT NULL DEFAULT(''),
 	content character varying(280) NOT NULL DEFAULT(''),
 	deadline timestamp without time zone,
-	CONSTRAINT user_pkey FOREIGN KEY (id) REFERENCES appUser(id)
+	CONSTRAINT user_pkey FOREIGN KEY (userid) REFERENCES appUser(id)
 )
 WITH(
 	OIDS=FALSE
@@ -14,8 +14,8 @@ WITH(
 
 CREATE INDEX ix_todolist_entryid
 	ON todolist
-	USING btree(entryid)
+	USING btree(entryid);
 
-CREATE INDEX ix_todolist_id
+CREATE INDEX ix_todolist_userid
 	ON todolist
-	USING btree(id)
+	USING btree(userid);
