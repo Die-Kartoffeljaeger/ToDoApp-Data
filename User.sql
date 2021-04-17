8ffed350c45c3e92700a4a29179d28b409ce435b
Jackson Bullard <jsbullar@uark.edu>
2021-04-17 12:42:48 -0500

    Update User.sql

--- User.sql
+++ User.sql
@@ -1,4 +1,4 @@
-CREATE TABLE appuser
+CREATE TABLE app_user
 (
 	id uuid NOT NULL DEFAULT gen_random_uuid(),
 	username character varying(32) NOT NULL DEFAULT(''),
@@ -13,7 +13,7 @@ WITH
 );
 
 CREATE INDEX ix_user_username
-	ON appuser
+	ON app_user
 	USING btree(username);
 
 -----
